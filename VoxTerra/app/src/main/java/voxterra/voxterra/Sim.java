6d6d7b8c9921a7668809fc6cc6ddc971ff157750
TMullCoding <thomasmullenwork@gmail.com>
2015-10-24 14:23:04 -0400

    A

---
+++ VoxTerra/app/src/main/java/voxterra/voxterra/Sim.java
@@ -0,0 +1,12 @@
+public class Sim {
+    private long globalPopulation;
+    private long waterLevel;
+
+    //Declare the continents
+    private long[] continents = new long[7];
+
+    //Declare the news stream
+    private NewsStream newsStream = new NewsStream();
+
+    //Get
+}
