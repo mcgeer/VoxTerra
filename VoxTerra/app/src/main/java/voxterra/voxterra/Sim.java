03ba10bc9fc6787b23eba291fe92a91c18044f69
TMullCoding <thomasmullenwork@gmail.com>
2015-10-24 14:49:56 -0400

    A

--- VoxTerra/app/src/main/java/voxterra/voxterra/Sim.java
+++ VoxTerra/app/src/main/java/voxterra/voxterra/Sim.java
@@ -1,10 +1,44 @@
 public class Sim {
     private long globalPopulation;
-    private long waterLevel;
 
     //Declare the continents
-    private long[] continents = new long[7];
+    private num_continents = 7;
+    private long[] continents = new long[num_continents];
+
+    //Global variables
+    private long co2_original = 100; //CHANGE THIS VALUE
+    private long co2 = co2_original;
+
+    private long temperature_original =
 
     //Getters
+    private long getWaterLevel(){
 
+    }
+
+    private int getTemperature(){
+        //Calculated from c02
+
+        /*Temperture goes up 2 degress for double C02!
+
+        "...the last IPCC report (AR4) described the likely range as between 2 and 4.5 degrees C,
+        for double the amount of CO2 compared to pre-industrial levels."
+        (https://www.skepticalscience.com/co2-temperature-correlation.htm) */
+    }
+
+    private long getNews(){
+
+    }
+
+    private long getCo2(){
+        return co2;
+    }
+
+    private long getWaterLevel(){
+
+    }
+
+    private Continent getContinentStat(name){
+
+    }
 }
