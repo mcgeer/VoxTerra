3a6567d878d7efaecf80fa98abd5982b4653f9ff
mcgeer <rileypatrickmcgee@gmail.com>
2015-10-24 20:27:46 -0400

    Added some not all needed functions for ShopItem and ResearchItem

--- VoxTerra/app/src/main/java/voxterra/voxterra/ResearchItem.java
+++ VoxTerra/app/src/main/java/voxterra/voxterra/ResearchItem.java
@@ -1,8 +1,58 @@
 package voxterra.voxterra;
 
 /**
- * Created by riley_000 on 2015-10-24.
+ *
  */
 public class ResearchItem {
+    private String name;
+    private String description;
 
+    private long cost;
+    private boolean unlocked;
+    private boolean purchased;
+
+    private ResearchItem[] child_items;
+    private ShopItem[] result_items;
+    public ResearchItem(String name, String description, long cost, boolean unlocked, ResearchItem[] child_items, ShopItem[] result_items){
+        this.name           = name;
+        this.description    = description;
+        this.cost           = cost;
+        this.unlocked       = unlocked;
+        this.purchased      = false;
+        this.child_items    = child_items;
+        this.result_items   = result_items;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public long getCost(){
+        return this.cost;
+    }
+
+
+    public boolean isUnlocked() {
+        return this.unlocked;
+    }
+
+    public void setUnlocked(boolean unlocked) {
+        this.unlocked = unlocked;
+    }
+
+    public void purchaseReseach(){
+        if(this.unlocked && !this.purchased){
+            this.purchased = true;
+            for(int i = 0; i < this.result_items.length; i++){
+                this.result_items[i].setUnlocked(true);
+            }
+            for(int i = 0; i < this.child_items.length; i++){
+                this.child_items[i].setUnlocked(true);
+            }
+        }
+    }
 }
--- VoxTerra/app/src/main/java/voxterra/voxterra/ShopItem.java
+++ VoxTerra/app/src/main/java/voxterra/voxterra/ShopItem.java
@@ -4,4 +4,30 @@ package voxterra.voxterra;
  * Created by riley_000 on 2015-10-24.
  */
 public class ShopItem {
+
+    private String name;
+    private String description;
+
+    private long cost;
+    private boolean unlocked;
+    private boolean purchased;
+
+    private ShopItem[] result_items;
+
+    public ShopItem(String name, String description, long cost, boolean unlocked, ShopItem[] result_items){
+        this.name           = name;
+        this.description    = description;
+        this.cost           = cost;
+        this.unlocked       = unlocked;
+        this.purchased      = false;
+        this.result_items   = result_items;
+    }
+
+    public boolean isUnlocked(){
+        return this.unlocked;
+    }
+
+    public void setUnlocked(boolean unlocked){
+        this.unlocked = unlocked;
+    }
 }
