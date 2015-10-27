aae509c175c12e93d01631d2c20ab89fc4948bec
mcgeer <rileypatrickmcgee@gmail.com>
2015-10-26 23:59:29 -0400

    Added Some comments for Sim and some methods

--- VoxTerra/app/src/main/java/voxterra/voxterra/Sim.java
+++ VoxTerra/app/src/main/java/voxterra/voxterra/Sim.java
@@ -5,21 +5,25 @@ package voxterra.voxterra;
  * Riley: I changed num continents to 1 for now!
  */
 public class Sim {
+    //=====================GLOBAL VARIABLES=====================//
     private static long global_population;
     //Declare the continents
     private static int num_continents = 1;
     //String name, long population,long emissions, int temperature, int MAX_WATER_LEVEL, double growth, double fresh_water, double relations, double stability
     private static Continent[] continents = {new Continent("North America",5287582631L,6672919291L,5,6,0.007,0.98,0.7,0.98)};
-
-    //Global variables
-    private static long co2_original = 100000; //CHANGE THIS VALUE
-    private static long co2 = co2_original;
+    private static long user_money      = 100000000L;
+    private static long co2_original    = 100000; //CHANGE THIS VALUE
+    private static long co2             = co2_original;
 
     private static int temperature_original = 6; //CHANGE THIS VALUE
 
     private static int water_level = 0;
 
-    //Getters
+    //======================GETTER METHODS======================//
+    public long getMoney(){
+        return this.user_money;
+    }
+
     public static int getWaterLevel(){
         return water_level;
     }
@@ -54,8 +58,17 @@ public class Sim {
         }
         return null;
     }
+    //======================SETTER METHODS======================//
+    private void increaseMoney(long money){
+        this.user_money += money;
+    }
 
-    public static void SimulateWeek(){
+    private void decreaseMoney(long money){
+        this.user_money -= money;
+    }
+    //======================STATE METHODS=======================//
+
+   public static void SimulateWeek(){
         //This needs to be called ever week, Make a timer for this say 5 seconds for test 30 seconds for game
         //Pause timer before calling this
         //Get the news and allow actions on it
@@ -67,15 +80,15 @@ public class Sim {
             Continent update_cont = continents[current_continent];
             update_cont.setPopulation((long)(update_cont.getPopulation()*update_cont.getGrowth()));
             update_cont.setEmissions((long)(update_cont.getEmissions()*update_cont.getGrowth()));
-            //update_cont.setFreshWater();
-            //update_cont.setRelations();
-            //update_cont.setStability();
-            //update_cont.setTemperature();
-            //update_cont.updateGrowth();
+            //update_cont.setFreshWater();  //Wont always change
+            //update_cont.setRelations();   //Wont always change
+            //update_cont.setStability();   //Wont always change
+            //update_cont.setTemperature(); //Will always change
+            //update_cont.updateGrowth();   //Will always change
         }
 
         //Increase money based on funding for environment and donations
-
+        //increaseMoney(FundraisedAmount);
         //Un-pause after return
 
         return;
